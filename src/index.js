1ee900f61a9006ca9c05a877f1e0bf767a3da5de
Edgar Montes <jedgar1mx@gmail.com>
2023-05-23 19:24:06 -0400

    Adding all components to build, Update setup storie to match new upcoming release.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,87 @@
-// // ============================================================
-// // Import to release
-// // ============================================================
-// // Importing atoms
-// import Alert from "./components/atoms/Alert/cod-alert";
-// import Badge from "./components/atoms/Badge/cod-badge";
-// import Button from "./components/atoms/Button/cod-button";
-// import Container from "./components/atoms/Container/cod-container";
-// import FormControl from "./components/atoms/FormControl/cod-formcontrol";
-// import FormLabel from "./components/atoms/FormLabel/cod-formlabel";
-// import FormSelect from "./components/atoms/FormSelect/cod-formselect";
-// import FormCheck from "./components/atoms/FormCheck/cod-formcheck";
-// import Icon from "./components/atoms/Icon/cod-icon";
-// import Image from "./components/atoms/Image/cod-image";
-// import Loader from "./components/atoms/Loader/cod-loader";
-// import Progress from "./components/atoms/Progress/cod-progress";
-// import Range from "./components/atoms/Range/cod-range";
-// import Spinner from "./components/atoms/Spinner/cod-spinner";
-
-// // Import molecules
-// import FormCheckGroup from "./components/molecules/FormCheckGroup/cod-form-check-group";
-
-// // Importing organisms
-// import Form from "./components/organisms/Form/cod-form";
-
 // ============================================================
-// Exporting to npm
+// Import to release
 // ============================================================
 // Importing atoms
-export * from "./components/atoms/Alert/cod-alert";
-export * from "./components/atoms/Badge/cod-badge";
-export * from "./components/atoms/Button/cod-button";
-export * from "./components/atoms/Container/cod-container";
-export * from "./components/atoms/FormControl/cod-formcontrol";
-export * from "./components/atoms/FormLabel/cod-formlabel";
-export * from "./components/atoms/FormSelect/cod-formselect";
-export * from "./components/atoms/FormCheck/cod-formcheck";
-export * from "./components/atoms/Icon/cod-icon";
-export * from "./components/atoms/Image/cod-image";
-export * from "./components/atoms/Loader/cod-loader";
-export * from "./components/atoms/Progress/cod-progress";
-export * from "./components/atoms/Range/cod-range";
-export * from "./components/atoms/Spinner/cod-spinner";
+import "./components/atoms/AccordionBody/cod-accordion-body";
+import "./components/atoms/AccordionHeader/cod-accordion-header";
+import "./components/atoms/AccordionItem/cod-accordion-item";
+import "./components/atoms/Alert/cod-alert";
+import "./components/atoms/Badge/cod-badge";
+import "./components/atoms/Breadcrumb/cod-breadcrumb";
+import "./components/atoms/Button/cod-button";
+import "./components/atoms/CardBody/cod-card-body";
+import "./components/atoms/CardFooter/cod-card-footer";
+import "./components/atoms/CardHeader/cod-card-header";
+import "./components/atoms/CardOverlay/cod-card-overlay";
+import "./components/atoms/CarouselCaption/cod-carousel-caption";
+import "./components/atoms/CarouselItem/cod-carousel-item";
+import "./components/atoms/Container/cod-container";
+import "./components/atoms/DropdownMenu/cod-dropdown-menu";
+import "./components/atoms/FormCheck/cod-formcheck";
+import "./components/atoms/FormControl/cod-formcontrol";
+import "./components/atoms/FormLabel/cod-formlabel";
+import "./components/atoms/FormSelect/cod-formselect";
+import "./components/atoms/Icon/cod-icon";
+import "./components/atoms/Image/cod-image";
+import "./components/atoms/Loader/cod-loader";
+import "./components/atoms/ModalBody/cod-modal-body";
+import "./components/atoms/ModalFooter/cod-modal-footer";
+import "./components/atoms/ModalHeader/cod-modal-header";
+import "./components/atoms/NavbarBrand/cod-navbar-brand";
+import "./components/atoms/NavbarCollapse/cod-navbar-collapse";
+import "./components/atoms/NavbarOffcanvas/cod-navbar-offcanvas";
+import "./components/atoms/NavbarToggle/cod-navbar-toggle";
+import "./components/atoms/OffcanvasBody/cod-offcanvas-body";
+import "./components/atoms/OffcanvasHeader/cod-offcanvas-header";
+import "./components/atoms/PaginationItem/cod-pagination-item";
+import "./components/atoms/Progress/cod-progress";
+import "./components/atoms/Range/cod-range";
+import "./components/atoms/Spinner/cod-spinner";
+import "./components/atoms/TableBody/cod-table-body";
+import "./components/atoms/TableCell/cod-table-cell";
+import "./components/atoms/TableCellHeader/cod-table-cell-header";
+import "./components/atoms/TableHeader/cod-table-header";
+import "./components/atoms/TableRow/cod-table-row";
 
 // Import molecules
-export * from "./components/molecules/FormCheckGroup/cod-form-check-group";
+import "./components/molecules/Accordion/cod-accordion";
+import "./components/molecules/ButtonGroup/cod-button-group";
+import "./components/molecules/Dropdown/cod-dropdown";
+import "./components/molecules/FormCheckGroup/cod-form-check-group";
+import "./components/molecules/ListGroup/cod-listgroup";
+import "./components/molecules/Nav/cod-nav";
+import "./components/molecules/Pagination/cod-pagination";
 
 // Importing organisms
-export * from "./components/organisms/Form/cod-form";
+import "./components/organisms/Card/cod-card";
+import "./components/organisms/Carousel/cod-carousel";
+import "./components/organisms/Form/cod-form";
+import "./components/organisms/Modal/cod-modal";
+import "./components/organisms/Navbar/cod-navbar";
+import "./components/organisms/Offcanvas/cod-offcanvas";
+import "./components/organisms/Table/cod-table";
+
+// // ============================================================
+// // Exporting to npm
+// // ============================================================
+// // Importing atoms
+// export * from "./components/atoms/Alert/cod-alert";
+// export * from "./components/atoms/Badge/cod-badge";
+// export * from "./components/atoms/Button/cod-button";
+// export * from "./components/atoms/Container/cod-container";
+// export * from "./components/atoms/FormControl/cod-formcontrol";
+// export * from "./components/atoms/FormLabel/cod-formlabel";
+// export * from "./components/atoms/FormSelect/cod-formselect";
+// export * from "./components/atoms/FormCheck/cod-formcheck";
+// export * from "./components/atoms/Icon/cod-icon";
+// export * from "./components/atoms/Image/cod-image";
+// export * from "./components/atoms/Loader/cod-loader";
+// export * from "./components/atoms/Progress/cod-progress";
+// export * from "./components/atoms/Range/cod-range";
+// export * from "./components/atoms/Spinner/cod-spinner";
+
+// // Import molecules
+// export * from "./components/molecules/FormCheckGroup/cod-form-check-group";
+
+// // Importing organisms
+// export * from "./components/organisms/Form/cod-form";
