533c15d301c4e39501c4bd951c0d6b3d3aa2bc18
Edgar Montes <jedgar1mx@gmail.com>
2023-06-12 19:22:54 -0400

    Adding fallback markup to Carousel component.

--- src/stories/carousel.stories.js
+++ src/stories/carousel.stories.js
@@ -18,6 +18,27 @@ export const Basic = () => html`
     <cod-carousel-item>
         <img src="https://placehold.co/800x400/a3a3a3/000" class="d-block w-100" alt="...">
     </cod-carousel-item>
+    <div id="carouselExample" class="no-wc carousel slide">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://placehold.co/800x400/000000/FFF" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="https://placehold.co/800x400/a9a9a9/fff" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="https://placehold.co/800x400/a3a3a3/000" class="d-block w-100" alt="...">
+        </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </cod-carousel>
 `;
 
