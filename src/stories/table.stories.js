03e6ac7c5e9f9d90b26e042e01e8b0993668952b
Edgar Montes <jedgar1mx@gmail.com>
2023-06-13 12:04:20 -0400

    Add fallback markup to Table component.

--- src/stories/table.stories.js
+++ src/stories/table.stories.js
@@ -12,6 +12,35 @@ export default {
 
 export const Basic = () => html`
 <cod-table data-id="basicTable">
+    <table class="table no-wc">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
     <cod-table-header>
         <cod-table-row>
         <cod-table-cell-header scope="col">#</cod-table-cell-header>
