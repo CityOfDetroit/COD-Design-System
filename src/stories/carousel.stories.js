ac648e0e02e54a8b1fa8e421f951a45388457670
Edgar Montes <jedgar1mx@gmail.com>
2023-08-03 12:55:20 -0400

    Adding external control option to carousel component.

--- src/stories/carousel.stories.js
+++ src/stories/carousel.stories.js
@@ -122,3 +122,29 @@ export const Dark = () => html`
     </cod-carousel-item>
 </cod-carousel>
 `;
+
+export const ExternalControls = () => html`
+<cod-carousel data-id="carouselExampleExternalControls" data-external-controls="true" data-extra-classes="carousel-dark">
+    <cod-carousel-item data-active="true">
+    <img src="https://placehold.co/800x400/a3a3a3/FFF" class="d-block w-100" alt="...">
+    <cod-carousel-caption data-extra-classes="text-dark">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+    </cod-carousel-caption>
+    </cod-carousel-item>
+    <cod-carousel-item>
+    <img src="https://placehold.co/800x400/a9a9a9/fff" class="d-block w-100" alt="...">
+    <cod-carousel-caption data-extra-classes="text-dark">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+    </cod-carousel-caption>
+    </cod-carousel-item>
+    <cod-carousel-item>
+    <img src="https://placehold.co/800x400/a3a3a3/000" class="d-block w-100" alt="...">
+    <cod-carousel-caption data-extra-classes="text-dark">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+    </cod-carousel-caption>
+    </cod-carousel-item>
+</cod-carousel>
+`;
