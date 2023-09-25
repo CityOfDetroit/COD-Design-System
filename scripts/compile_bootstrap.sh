03f04076237f608a18ec5f0d6b5db7755aaa3911
Max Morgan <maxwell.morgan@detroitmi.gov>
2023-09-25 14:45:26 -0400

    Use EMs in UXDS and add script for CSS compilation

---
+++ scripts/compile_bootstrap.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DEFAULT_PROJECT_ROOT=./
+PROJECT_ROOT=${1:-$DEFAULT_PROJECT_ROOT}
+
+if [[ ! -d $PROJECT_ROOT ]];
+then
+    echo ${PROJECT_ROOT}" is not a directory..."
+    exit 1
+fi
+
+compile_sass () {
+    echo "Compiling bootstrap SASS..."
+    yarn sass ${PROJECT_ROOT}src/scss/themed-bootstrap.scss ${PROJECT_ROOT}src/shared/themed-bootstrap.css
+    if [ $? -ne 0 ];
+    then
+        echo "Failed to compile bootstrap... Aborting."
+        exit 1
+    fi
+}
+
+replace_rem_w_em () {
+    echo "Replacing REMs with EMs..."
+    sed -i -r "s|([0-9])rem|\1em|g" ${PROJECT_ROOT}src/shared/themed-bootstrap.css
+    if [ $? -ne 0 ];
+    then
+        echo "Failed to replace content... Aborting."
+        exit 1
+    fi
+}
+
+format_bootstrap () {
+    echo "Formatting output..."
+    yarn prettier -w ${PROJECT_ROOT}src/shared/themed-bootstrap.css
+    if [ $? -ne 0 ];
+    then
+        echo "Formatting output failed... Aborting."
+        exit 1
+    fi
+}
+
+compile_sass
+replace_rem_w_em
+format_bootstrap
+exit $?
