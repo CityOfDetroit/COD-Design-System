ce7612d87e0e98e7db9d462204d125279448f39c
Max Morgan <maxwell.morgan@detroitmi.gov>
2024-01-12 17:14:38 -0500

    Add mixin for observed attributes

---
+++ src/shared/js/observed-attribute-mixin.js
@@ -0,0 +1,18 @@
+const observedAttributeMixin = {
+  /**
+   * Handle an observed attribute change that controls a class on an
+   * HTML element:
+   *  codElement.handleObservedClassAttribute(
+   *    newValue, // undefined or 'table-stacked'
+   *    this.tableHeader, // the HTMLElement where the class is applied
+   *    'table-stacked' // the name of the class to add
+   *  )
+   */
+  handleObservedClassAttribute(newValue, htmlElement, className) {
+    if (newValue) {
+      htmlElement.classList.add(className);
+    } else {
+      htmlElement.classList.remove(className);
+    }
+  },
+};
