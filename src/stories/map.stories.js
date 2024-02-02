50d16486f207b16d9dfc8384059ea007f9bcb93a
Edgar Montes <jedgar1mx@gmail.com>
2024-02-02 15:31:55 -0500

    Removing Map changes from this feature branch.

--- src/stories/map.stories.js
+++
@@ -1,8 +0,0 @@
-import { html } from 'lit-html';
-import '../components/organisms/Map/cod-map';
-
-export default {
-  title: 'Components/Organisms/Map',
-};
-
-export const loaded = () => html`<cod-map data-location="{&quot;address&quot;:&quot;1104 Military St, Detroit, MI, 48209&quot;,&quot;location&quot;:{&quot;x&quot;:-83.103111,&quot;y&quot;:42.31103400000001}}" data-map-state="loaded"></cod-map>`;
