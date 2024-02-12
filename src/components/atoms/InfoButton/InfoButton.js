4b0027fbb0f04f4a8ddf2c72621e4cb9b15ce8ff
Max Morgan <maxwell.morgan@detroitmi.gov>
2024-02-12 17:18:51 -0500

    Add arpa button

---
+++ src/components/atoms/InfoButton/InfoButton.js
@@ -0,0 +1,109 @@
+import styles from '!!raw-loader!./InfoButton.css';
+import varStyles from '!!raw-loader!../../../shared/variables.css';
+import bootstrapStyles from '!!raw-loader!../../../shared/themed-bootstrap.css';
+
+const template = document.createElement('template');
+template.innerHTML = `
+<slot></slot>
+`;
+
+class InfoButton extends HTMLElement {
+  static observedAttributes = [];
+
+  constructor() {
+    // Always call super first in constructor
+    super();
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(template.content.cloneNode(true));
+    this.div = document.createElement('div');
+    shadow.addEventListener('slotchange', (ev) => {
+      const tempElements = ev.target.assignedElements();
+      tempElements.forEach((node) => {
+        this.div.append(node);
+      });
+    });
+
+    // Add styles
+    const bootStyles = document.createElement('style');
+    bootStyles.textContent = bootstrapStyles;
+    const variableStyles = document.createElement('style');
+    variableStyles.textContent = varStyles;
+    const itemStyles = document.createElement('style');
+    itemStyles.textContent = styles;
+    shadow.appendChild(bootStyles);
+    shadow.appendChild(variableStyles);
+    shadow.appendChild(itemStyles);
+  }
+
+  connectedCallback() {
+    // Build the img for the top of the card.
+    const imgSrc = this.getAttribute('img-src');
+    const imgAlt = this.getAttribute('img-alt');
+    const imgElt = document.createElement('img');
+    imgElt.setAttribute('src', imgSrc);
+    imgElt.setAttribute('alt', imgAlt);
+    imgElt.classList.add('card-img-top', 'img-fluid');
+
+
+    // Build the title section.
+    // TODO: Make title-primary conditional.
+    const titlePrim = this.getAttribute('title-primary');
+    const titlePrimElt = document.createElement('h5');
+    titlePrimElt.innerText = titlePrim;
+
+    // TODO: Make title-secondary conditional.
+    const titleSec = this.getAttribute('title-secondary');
+    const titleSecElt = document.createElement('h5');
+    titleSecElt.innerText = titleSec;
+    [titlePrimElt, titleSecElt].forEach((elt) => elt.classList.add('info-btn-title'));
+    titleSecElt.classList.add('text-warning', 'text-end');
+
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('d-flex', 'justify-content-between');
+    titleContainer.appendChild(titlePrimElt);
+    titleContainer.appendChild(titleSecElt);
+
+    // Build the body section.
+    const body = this.getAttribute('body');
+    const bodyElt = document.createElement('p');
+    bodyElt.classList.add('card-text', 'pe-3');
+    bodyElt.innerText = body;
+    const chevronElt = document.createElement('cod-icon');
+    chevronElt.setAttribute('data-icon', 'chevron-right-circle');
+    chevronElt.setAttribute('data-size', 'medium');
+    const bodyContainer = document.createElement('div');
+    bodyContainer.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    bodyContainer.appendChild(bodyElt);
+    bodyContainer.appendChild(chevronElt);
+
+    // Build the card body.
+    const cardBodyElt = document.createElement('div');
+    cardBodyElt.classList.add('card-body', 'text-light');
+    cardBodyElt.appendChild(titleContainer);
+    cardBodyElt.appendChild(bodyContainer);
+
+    // Build the card.
+    const cardElt = document.createElement('div');
+    cardElt.classList.add('card');
+    cardElt.appendChild(imgElt);
+    cardElt.appendChild(cardBodyElt);
+
+    // Build clickable wrapper.
+    const href = this.getAttribute('href');
+    const target = this.getAttribute('target');
+    const clickableContainer = document.createElement('a');
+    clickableContainer.classList.add('btn', 'btn-primary');
+    clickableContainer.setAttribute('role', 'button');
+    clickableContainer.setAttribute('href', href);
+    if (target) {
+      clickableContainer.setAttribute('target', target);
+    }
+    clickableContainer.appendChild(cardElt);
+
+    this.div.appendChild(clickableContainer);
+    this.shadowRoot.appendChild(this.div);
+  }
+}
+
+export { InfoButton as default };
---
+++ src/stories/infobutton.stories.js
@@ -0,0 +1,50 @@
+import '../components/organisms/Card/cod-card';
+import '../components/atoms/CardBody/cod-card-body';
+import '../components/atoms/Icon/cod-icon';
+import '../components/atoms/Clickable/cod-clickable';
+import '../components/atoms/InfoButton/cod-info-button';
+
+export default {
+  component: 'cod-info-button',
+  title: 'Components/Atoms/InfoButton',
+  // 👇 Creates specific argTypes
+  argTypes: {
+    titlePrimary: {
+      control: 'text',
+      description:
+        '(Optional) The text to be used for a primary title. \
+        If the attribute is not included or empty string, then the \
+        title is omitted.',
+    },
+    titleSecondary: {
+      control: 'text',
+      description:
+        '(Optional) The text to be used for a secondary title. \
+        If the attribute is not included or empty string, and the primary \
+        title is also ommitted, then the entire title row is omitted.',
+    },
+    body: {
+      control: 'text',
+      description: 'The text to be used for the body of the button.',
+    },
+  },
+  args: {
+    titlePrimary: 'See This Info Button',
+    titleSecondary: '$90 million',
+    body: "This is a longer piece of content but still brief since it's a button.",
+  },
+};
+// Template
+const Template = (args) => {
+  const infoBtn = document.createElement('cod-info-button');
+  infoBtn.setAttribute('img-src', 'https://placehold.co/800x400/000000/FFF');
+  infoBtn.setAttribute('img-alt', 'A placeholder image');
+  infoBtn.setAttribute('href', 'https://example.com');
+  infoBtn.setAttribute('target', '_blank');
+  infoBtn.setAttribute('title-primary', args.titlePrimary);
+  infoBtn.setAttribute('title-secondary', args.titleSecondary);
+  infoBtn.setAttribute('body', args.body);
+  return infoBtn;
+};
+
+export const Primary = Template.bind({});
