ebbe4538eff74f18d795730ba49fa56c7203e1ec
Max Morgan <maxwell.morgan@detroitmi.gov>
2024-03-05 14:02:13 -0500

    Fix formatting

--- src/stories/slider.stories.js
+++ src/stories/slider.stories.js
@@ -8,20 +8,22 @@ export default {
 };
 
 export const Basic = () => html`
-<cod-slider label-text="Use the slider to control the visibility of the two images.">
-<img 
-  slot="image-1" 
-  alt='A friendly illustrated cloud character with eyes on a red-pink background.' 
-  src="${img1}"
-  width="1600" 
-  height="900"
-/>
-<img 
-  slot="image-2" 
-  alt='A friendly illustrated cloud character with eyes surrounded by a variety of icons (including check marks, stars, code brackets, buttons, hearts, a checkbox, and many more) on a blue background.' 
-  src="${img2}"  
-  width="1600" 
-  height="900"        
-/>
-</cod-slider>
+  <cod-slider
+    label-text="Use the slider to control the visibility of the two images."
+  >
+    <img
+      slot="image-1"
+      alt="A friendly illustrated cloud character with eyes on a red-pink background."
+      src="${img1}"
+      width="1600"
+      height="900"
+    />
+    <img
+      slot="image-2"
+      alt="A friendly illustrated cloud character with eyes surrounded by a variety of icons (including check marks, stars, code brackets, buttons, hearts, a checkbox, and many more) on a blue background."
+      src="${img2}"
+      width="1600"
+      height="900"
+    />
+  </cod-slider>
 `;
