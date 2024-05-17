7089ad990897d76c7ba2268da0286d7c9e06bdbb
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2024-05-17 11:55:26 -0400

    Added data- back to the closeBtn component for now

--- src/components/atoms/OffcanvasHeader/OffcanvasHeader.js
+++ src/components/atoms/OffcanvasHeader/OffcanvasHeader.js
@@ -47,13 +47,13 @@ export default class OffcanvasHeader extends HTMLElement {
     const offcanvasHeaderClasses = ['offcanvas-header'];
     this.offcanvasTitle.className = 'offcanvas-title';
     this.offcanvasTitle.id = `${parentID}-label`;
-    this.closeBtn.setAttribute('img-alt', '');
-    this.closeBtn.setAttribute('icon', '');
-    this.closeBtn.setAttribute('close', 'true');
-    this.closeBtn.setAttribute('bs-dismiss', parentID);
+    this.closeBtn.setAttribute('data-img-alt', '');
+    this.closeBtn.setAttribute('data-icon', '');
+    this.closeBtn.setAttribute('data-close', 'true');
+    this.closeBtn.setAttribute('data-bs-dismiss', parentID);
     extraClasses ? offcanvasHeaderClasses.push(extraClasses) : 0;
     btnDark === 'true'
-      ? this.closeBtn.setAttribute('extra-classes', 'btn-close-white')
+      ? this.closeBtn.setAttribute('data-extra-classes', 'btn-close-white')
       : 0;
     const expand = this.getAttribute('data-expand');
     if (expand) {
