b1fe835fa43dde4ae7ddbbcd5568c69d6ec7f133
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2024-07-15 15:46:39 -0400

    Updated attributes in Offcanvas example

--- src/stories/navbar.stories.js
+++ src/stories/navbar.stories.js
@@ -454,13 +454,13 @@ export const Offcanvas = () => html`
           <a class="nav-link" href="#">Link</a>
           <cod-dropdown data-split="false">
             <cod-button
-              data-label="Dropdown"
-              data-primary="true"
-              data-img-alt=""
-              data-icon=""
+              label="Dropdown"
+              primary="true"
+              img-alt=""
+              icon=""
               data-bs-toggle="dropdown"
               aria-expanded="false"
-              data-extra-classes="dropdown-toggle text-primary"
+              extra-classes="dropdown-toggle text-primary"
             ></cod-button>
             <cod-dropdown-menu>
               <li><a class="dropdown-item" href="#">Link</a></li>
