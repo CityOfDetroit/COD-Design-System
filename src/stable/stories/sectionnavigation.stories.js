418caf88f9f2b44429bfa8ddadb1ea43e39a9842
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-01-17 09:48:27 -0500

    Updated stories file, SectionNavigation.css and fixed prettier issues in 2 files

--- src/stable/stories/sectionnavigation.stories.js
+++ src/stable/stories/sectionnavigation.stories.js
@@ -9,15 +9,13 @@ export default {
 export const Default = {
   render: () => html`
     <cod-section-navigation>
-      <nav slot="nav-items">
-        <a href="#services">Services</a>
-        <a href="#council-sessions">Council Sessions</a>
-        <a href="#community-events">Community Events</a>
-        <a href="#news">News</a>
-        <a href="#council-members">Council Members</a>
-        <a href="#standing-committees">Standing Committees</a>
-        <a href="#departments">Departments, Contacts & More</a>
-      </nav>
+      <a slot="nav-items" href="#services">Services</a>
+      <a slot="nav-items" href="#council-sessions">Council Sessions</a>
+      <a slot="nav-items" href="#community-events">Community Events</a>
+      <a slot="nav-items" href="#news">News</a>
+      <a slot="nav-items" href="#council-members">Council Members</a>
+      <a slot="nav-items" href="#standing-committees">Standing Committees</a>
+      <a slot="nav-items" href="#departments">Departments, Contacts & More</a>
     </cod-section-navigation>
   `,
 };
