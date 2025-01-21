410113f517d50886b4518a2625e06e383b0fe4b7
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-01-21 11:05:51 -0500

    Removed template code from govbanner.stories.js

--- src/stable/stories/govbanner.stories.js
+++ src/stable/stories/govbanner.stories.js
@@ -9,37 +9,6 @@ export default {
 export const Default = {
   render: () => html`
     <cod-gov-banner>
-      <span slot="city-name">CITY OF DETROIT</span>
-      <span slot="official-statement"
-        >An official website of the City of Detroit.</span
-      >
-      <span slot="know-statement">Here's how you know.</span>
-      <span slot="toggle-icon">▼</span>
-      <div slot="content">
-        <div class="info-section">
-          <div class="info-item">
-            <div class="icon">🏛️</div>
-            <div>
-              <span class="info-title">Official websites use .gov</span>
-              <p>
-                A .gov website belongs to an official government organization in
-                the United States.
-              </p>
-            </div>
-          </div>
-          <div class="info-item">
-            <div class="icon">🔒</div>
-            <div>
-              <span class="info-title">Secure .gov websites use HTTPS</span>
-              <p>
-                A lock or https:// means you've safely connected to the .gov
-                website. Share sensitive information only on official, secure
-                websites.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
     </cod-gov-banner>
   `,
 };
