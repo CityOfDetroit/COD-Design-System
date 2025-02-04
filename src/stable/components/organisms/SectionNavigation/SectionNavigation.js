cc3947f2a8e86d75952b202aafbf13b22730ffa2
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-02-04 14:47:14 -0500

    Changed h2 header into a slot and updated stories and CSS file

--- src/stable/components/organisms/SectionNavigation/SectionNavigation.js
+++ src/stable/components/organisms/SectionNavigation/SectionNavigation.js
@@ -11,7 +11,7 @@ ${styles}
 </style>
 <div class="section-container">
   <div class="section-header">
-    <h2>On This Page</h2>
+    <slot name="header">On This Page</slot>
     <button type="button" class="toggle-button" aria-label="Toggle navigation" aria-expanded="false">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
