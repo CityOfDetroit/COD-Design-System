4299545c506b1b81471fd9d6569bac1d3dab6b07
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-02-05 14:02:07 -0500

    Fix stylings for header and nav-links

--- src/stable/stories/sectionnavigation.stories.js
+++ src/stable/stories/sectionnavigation.stories.js
@@ -8,15 +8,20 @@ export default {
 
 export const Default = {
   render: () => html`
+  <style>
+    .section-nav-link {
+    text-decoration: none;
+    }
+  </style>
     <cod-section-navigation>
-      <h2 slot="header">On This Page</h2>
-      <a slot="nav-items" href="#services">Services</a>
-      <a slot="nav-items" href="#council-sessions">Council Sessions</a>
-      <a slot="nav-items" href="#community-events">Community Events</a>
-      <a slot="nav-items" href="#news">News</a>
-      <a slot="nav-items" href="#council-members">Council Members</a>
-      <a slot="nav-items" href="#standing-committees">Standing Committees</a>
-      <a slot="nav-items" href="#departments">Departments, Contacts & More</a>
+      <span slot="header">On This Page</span>
+      <a slot="nav-items" href="#services" class="section-nav-link">Services</a>
+      <a slot="nav-items" href="#council-sessions" class="section-nav-link">Council Sessions</a>
+      <a slot="nav-items" href="#community-events" class="section-nav-link">Community Events</a>
+      <a slot="nav-items" href="#news" class="section-nav-link">News</a>
+      <a slot="nav-items" href="#council-members" class="section-nav-link">Council Members</a>
+      <a slot="nav-items" href="#standing-committees" class="section-nav-link">Standing Committees</a>
+      <a slot="nav-items" href="#departments" class="section-nav-link">Departments, Contacts & More</a>
     </cod-section-navigation>
   `,
 };
