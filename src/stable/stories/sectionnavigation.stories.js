7233119af4b7cb09a770de805ba817e7b3d55522
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-02-05 14:02:51 -0500

    Fixed prettier issues in 1 file

--- src/stable/stories/sectionnavigation.stories.js
+++ src/stable/stories/sectionnavigation.stories.js
@@ -8,20 +8,30 @@ export default {
 
 export const Default = {
   render: () => html`
-  <style>
-    .section-nav-link {
-    text-decoration: none;
-    }
-  </style>
+    <style>
+      .section-nav-link {
+        text-decoration: none;
+      }
+    </style>
     <cod-section-navigation>
       <span slot="header">On This Page</span>
       <a slot="nav-items" href="#services" class="section-nav-link">Services</a>
-      <a slot="nav-items" href="#council-sessions" class="section-nav-link">Council Sessions</a>
-      <a slot="nav-items" href="#community-events" class="section-nav-link">Community Events</a>
+      <a slot="nav-items" href="#council-sessions" class="section-nav-link"
+        >Council Sessions</a
+      >
+      <a slot="nav-items" href="#community-events" class="section-nav-link"
+        >Community Events</a
+      >
       <a slot="nav-items" href="#news" class="section-nav-link">News</a>
-      <a slot="nav-items" href="#council-members" class="section-nav-link">Council Members</a>
-      <a slot="nav-items" href="#standing-committees" class="section-nav-link">Standing Committees</a>
-      <a slot="nav-items" href="#departments" class="section-nav-link">Departments, Contacts & More</a>
+      <a slot="nav-items" href="#council-members" class="section-nav-link"
+        >Council Members</a
+      >
+      <a slot="nav-items" href="#standing-committees" class="section-nav-link"
+        >Standing Committees</a
+      >
+      <a slot="nav-items" href="#departments" class="section-nav-link"
+        >Departments, Contacts & More</a
+      >
     </cod-section-navigation>
   `,
 };
