5713e1d51a2eb8cec64478df13fae4e2b17ee1a6
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-02-12 14:52:42 -0500

    Fixed prettier issues in 5 files

--- src/stable/stories/servicebutton.stories.js
+++ src/stable/stories/servicebutton.stories.js
@@ -2,15 +2,17 @@ import { html } from 'lit-html';
 import '../../stable/components/ServiceButton/cod-service-button';
 
 export default {
-    title: 'Stable/ServiceButton',
-    tags: ['autodocs'],
+  title: 'Stable/ServiceButton',
+  tags: ['autodocs'],
 };
 
 export const Default = {
-    render: (args) => html `
-        <cod-service-button>
-            <span slot="title">Apply for a Job</span>
-            <span slot="subtitle">View job postings for the City of Detroit or our partners.</span>
-        </cod-service-button>
-    `
+  render: () => html`
+    <cod-service-button>
+      <span slot="title">Apply for a Job</span>
+      <span slot="subtitle"
+        >View job postings for the City of Detroit or our partners.</span
+      >
+    </cod-service-button>
+  `,
 };
