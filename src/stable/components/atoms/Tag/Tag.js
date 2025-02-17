271822f04306540b0266c2d01c5b4379d5b98319
Shannon <shannon.brownlee@detroitmi.gov>
2025-02-17 08:55:13 -0500

    updates storybook structure and other review notes

---
+++ src/stable/components/atoms/Tag/Tag.js
@@ -0,0 +1,58 @@
+import styles from '!!raw-loader!./tag.css';
+
+// Function to strip HTML tags
+function stripHTML(html) {
+  const doc = new DOMParser().parseFromString(html, 'text/html');
+  return doc.body.textContent || "";
+}
+
+// Define a template element to hold the structure and style of the custom element
+const template = document.createElement('template');
+template.innerHTML = `
+  <div class="tag-container">
+  <style>
+    ${styles}
+  </style>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
+        <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+        <path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z"/>
+      </svg>
+    <slot name="label"></slot>
+  </div>
+`;
+
+class Tag extends HTMLElement {
+  constructor() {
+    super();
+    // Create a shadow root and append the template content to it
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    // Setup slot event listener when the element is added to the DOM
+    this._setupSlot();
+  }
+
+  _setupSlot() {
+    // Find the slot element and add an event listener for the slotchange event
+    const slot = this.shadowRoot.querySelector('slot[name="label"]');
+    slot.addEventListener('slotchange', () => {
+      const elements = slot.assignedElements(); // Get the elements assigned to the slot
+      elements.forEach(element => {
+        // Check if the element is not a <span> or <a>
+        if (element.tagName !== 'SPAN' && element.tagName !== 'A') {
+          // Strip inner HTML content
+          element.innerHTML = stripHTML(element.innerHTML);
+        }
+      });
+    });
+  }
+
+  isTagConnected() {
+    // Check if the tag-container is connected to the shadow DOM
+    return this.shadowRoot.querySelector('.tag-container') !== null;
+  }
+}
+
+export default Tag;
---
+++ src/stable/stories/tag.stories.js
@@ -0,0 +1,28 @@
+import '../components/atoms/Tag/cod-tag';
+
+export default {
+  title: 'Stable/components/Atoms/Tag',
+  argTypes: {
+    label: { control: 'text', defaultValue: 'Label' },
+  },
+};
+
+// Template
+const Template = (args) => {
+  const tag = document.createElement('cod-tag');
+
+  // Create a span element for the label, set its slot name and text content
+  const label = document.createElement('span');
+  label.slot = 'label';
+  label.textContent = args.label;
+  
+  // Append the label to the tag element
+  tag.appendChild(label);
+  
+  return tag;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "Mayor's Office",
+};
