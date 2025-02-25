b54e7a6d3ca91b8ac3d5a6aaa7242073f36a2693
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-02-25 11:09:19 -0500

    Pushing 5th test

--- src/stable/stories/sectionnavigation.stories.js
+++ src/stable/stories/sectionnavigation.stories.js
@@ -99,14 +99,14 @@ export const Default = {
     await userEvent.click(toggleButton);
     checkExpandedState(false);
 
-  //   // Verify navigation items
-  //   await waitFor(() => {
-  //     const slot = shadow.querySelector('slot[name="nav-items"]');
-  //     const navItems = slot.assignedElements();
-  //     expect(navItems.length).toBe(7);
-  //     expect(navItems[0].textContent).toBe('Services');
-  //     expect(navItems[0].getAttribute('href')).toBe('#services');
-  //   });
+    // Verify navigation items
+    await waitFor(() => {
+      const slot = shadow.querySelector('slot[name="nav-items"]');
+      const navItems = slot.assignedElements();
+      expect(navItems.length).toBe(7);
+      expect(navItems[0].textContent).toBe('Services');
+      expect(navItems[0].getAttribute('href')).toBe('#services');
+    });
 
   //   // test slotted <a> elements are wrapped in <li>
   //   await waitFor(() => {
