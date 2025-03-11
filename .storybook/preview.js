9416d4f532932fae5a8fcda6915eb46fe295cab3
Maxwell Morgan <maxwell.morgan@detroitmi.gov>
2025-03-11 12:24:10 -0400

    Do not use deprecated action shim

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: {},
   controls: {
     matchers: {
       color: /(background|color)$/i,
