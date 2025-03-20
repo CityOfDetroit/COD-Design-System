b3286982ea6d47ccb70441f9252e9f1ea559137f
Maxwell Morgan <maxwell.morgan@detroitmi.gov>
2025-03-20 12:11:59 -0400

    Rename introduction to home

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,7 +11,7 @@ export const parameters = {
   },
   options: {
     storySort: {
-      order: ['Getting Started', 'Components', 'Experimental'],
+      order: ['Getting Started', ['Home'], 'Components', 'Experimental'],
     },
   },
 };
