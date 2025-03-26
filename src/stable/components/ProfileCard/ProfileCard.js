eb6cd79d0508f0700080f331384f5c10ae76b275
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-03-26 16:15:33 -0400

    Created full profile-card component with completed javascript

--- src/stable/components/ProfileCard/ProfileCard.js
+++ src/stable/components/ProfileCard/ProfileCard.js
@@ -6,8 +6,13 @@ template.innerHTML = `
 ${styles}
 </style>
 
-<div>
-<p> PROFILE CARD HERE </p>
+<div class="profile-card">
+ <img class="profile-image" alt="Profile Image">
+ <div class="profile-details">
+ <slot name="name"></slot>
+ <slot name="title-primary"></slot>
+ <slot name="title-secondary"></slot>
+ </div>
 </div>
 `;
 
@@ -17,6 +22,42 @@ class ProfileCard extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.appendChild(template.content.cloneNode(true));
   }
+
+  static get observedAttributes() {
+    return ['image-src'];
+  }
+
+  attributeChangedCallback(name, newValue) {
+    if (name === 'image-src') {
+      this._updateImage(newValue);
+    }
+  }
+
+  connectedCallback() {
+    this._updateImage(this.getAttribute('image-src'));
+    this._validateNameSlot();
+  }
+
+  _updateImage(newValue) {
+    const img = this.shadowRoot.querySelector('.profile-image');
+    if (img) {
+      img.src = newValue || '';
+    }
+  }
+
+  _validateNameSlot() {
+    const slot = this.shadowRoot.querySelector('slot[name="name"]');
+    slot.addEventListener('slotchange', () => {
+      const assignedNodes = slot.assignedNodes({ flatten: true });
+      if(assignedNodes.length > 0) {
+        const assignedElement = assignedNodes[0];
+        if(!(assignedElement instanceof HTMLSpanElement || assignedElement instanceof HTMLAnchorElement)){
+          console.warn('ProfileCard: The "name" slot should contain either a <span> or an <a> element.');
+        }
+      }
+    });
+  }
+
 }
 
 export { ProfileCard as default };
