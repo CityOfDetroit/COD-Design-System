93527aaa9c9ad5177a8c55fbb3ec0f5b2ec9b1ec
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-04-17 10:41:23 -0400

    Removed call to validateSlots function from connectedcallback, removed commented out profilecard code, added a name-container around name and chevron, updated scss to fix position of chevron

--- src/stable/components/ProfileCard/ProfileCard.js
+++ src/stable/components/ProfileCard/ProfileCard.js
@@ -1,104 +1,3 @@
-// import styles from '!!raw-loader!./ProfileCard.css';
-
-// const template = document.createElement('template');
-// template.innerHTML = `
-// <style>
-// ${styles}
-// </style>
-// <a class="profile-card">
-//  <img class="profile-image" alt="Profile Image">
-//  <div class="profile-details">
-//  <slot name="name"></slot>
-//  <slot name="title-primary"></slot>
-//  <slot name="title-secondary"></slot>
-//  </div>
-// </a>
-// `;
-// class ProfileCard extends HTMLElement {
-//   constructor() {
-//     super();
-//     const shadow = this.attachShadow({ mode: 'open' });
-//     shadow.appendChild(template.content.cloneNode(true));
-//   }
-//   static get observedAttributes() {
-//     return ['src', 'href'];
-//   }
-//   attributeChangedCallback(name, oldValue, newValue) {
-//     if (name === 'src') {
-//       this._updateImage(newValue);
-//     }
-//     if (name === 'href') {
-//       this._updateLink(newValue);
-//     }
-//   }
-//   connectedCallback() {
-//     this._updateImage(this.getAttribute('src'));
-//     this._updateLink(this.getAttribute('href'));
-//     this._validateNameSlot();
-//   }
-
-//   getValidationError() {
-//     return this._validationError;
-//   }
-
-//   _updateImage(newValue) {
-//     const img = this.shadowRoot.querySelector('.profile-image');
-//     if (img) {
-//       img.src = newValue || '';
-//     }
-//   }
-
-//   _validateNameSlot() {
-//     const slot = this.shadowRoot.querySelector('slot[name="name"]');
-//     if (slot) {
-//       slot.addEventListener('slotchange', () => {
-//         this._validateSlotContent();
-//       });
-
-//       this._validateSlotContent();
-//     }
-//   }
-
-//   _validateSlotContent() {
-//     const slot = this.shadowRoot.querySelector('slot[name="name"]');
-//     if (!slot) return;
-
-//     const assignedNodes = slot.assignedNodes({ flatten: true });
-
-//     if (assignedNodes.length > 0) {
-//       const firstElement = assignedNodes.find(
-//         (node) => node.nodeType === Node.ELEMENT_NODE,
-//       );
-
-//       if (
-//         firstElement &&
-//         !(
-//           firstElement instanceof HTMLSpanElement ||
-//           firstElement instanceof HTMLAnchorElement
-//         )
-//       ) {
-//         throw new Error(
-//           'ProfileCard: The "name" slot should contain either a <span> or an <a> element.',
-//         );
-//       }
-//     }
-//   }
-
-//   _updateLink(newValue) {
-//     const card = this.shadowRoot.querySelector('.profile-card');
-//     if (card) {
-//       if (newValue) {
-//         card.href = newValue;
-//         card.target = '_blank';
-//         card.rel = 'noopener noreferrer';
-//       } else {
-//         card.removeAttribute('href');
-//       }
-//     }
-//   }
-// }
-// export { ProfileCard as default };
-
 import styles from '!!raw-loader!./ProfileCard.css';
 
 const template = document.createElement('template');
@@ -109,7 +8,10 @@ ${styles}
 <a class="profile-card">
   <img class="profile-image" alt="Profile Image">
   <div class="profile-details">
-    <slot name="name"></slot>
+    <div class="name-container">
+      <slot name="name"></slot>
+      <span class="chevron">›</span>
+    </div>
     <slot name="title-primary"></slot>
     <slot name="title-secondary"></slot>
   </div>
@@ -155,7 +57,6 @@ class ProfileCard extends HTMLElement {
   connectedCallback() {
     this._updateImage(this.src);
     this._updateLink(this.href);
-    this._validateNameSlot();
   }
 
   _updateImage(newValue) {
