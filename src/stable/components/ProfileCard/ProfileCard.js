0daceaaeaabe8a905113558a989eaadd41ec752c
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-04-17 09:44:11 -0400

    Removed validate slot methods and sanity checks

--- src/stable/components/ProfileCard/ProfileCard.js
+++ src/stable/components/ProfileCard/ProfileCard.js
@@ -181,42 +181,6 @@ class ProfileCard extends HTMLElement {
       }
     }
   }
-
-  _validateNameSlot() {
-    const slot = this.shadowRoot.querySelector('slot[name="name"]');
-    if (slot) {
-      slot.addEventListener('slotchange', () => {
-        this._validateSlotContent();
-      });
-
-      this._validateSlotContent();
-    }
-  }
-
-  _validateSlotContent() {
-    const slot = this.shadowRoot.querySelector('slot[name="name"]');
-    if (!slot) return;
-
-    const assignedNodes = slot.assignedNodes({ flatten: true });
-
-    if (assignedNodes.length > 0) {
-      const firstElement = assignedNodes.find(
-        (node) => node.nodeType === Node.ELEMENT_NODE,
-      );
-
-      if (
-        firstElement &&
-        !(
-          firstElement instanceof HTMLSpanElement ||
-          firstElement instanceof HTMLAnchorElement
-        )
-      ) {
-        throw new Error(
-          'ProfileCard: The "name" slot should contain either a <span> or an <a> element.',
-        );
-      }
-    }
-  }
 }
 
 export { ProfileCard as default };
