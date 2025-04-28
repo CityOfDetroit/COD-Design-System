1c0eb2db2e08c1bbf0b55831f2d26d147002d3b6
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-04-28 15:40:28 -0400

    Exposed target and rel attributes

--- src/stable/components/ProfileCard/ProfileCard.js
+++ src/stable/components/ProfileCard/ProfileCard.js
@@ -28,23 +28,20 @@ class ProfileCard extends HTMLElement {
     this._href = this.getAttribute('href') || '';
     this._src = this.getAttribute('src') || '';
     this._alt = this.getAttribute('alt') || 'Profile Image';
+    this._target = this.getAttribute('target') || '_blank'; 
+    this._rel = this.getAttribute('rel') || 'noopener noreferrer'; 
   }
 
   static get observedAttributes() {
-    return ['src', 'href', 'alt'];
+    return ['src', 'href', 'alt', 'target', 'rel'];
   }
 
-  get href() {
-    return this._href;
-  }
 
-  get src() {
-    return this._src;
-  }
-
-  get alt() {
-    return this._alt;
-  }
+  get href() { return this._href; }
+  get src() { return this._src; }
+  get alt() { return this._alt; }
+  get target() { return this._target; }
+  get rel() { return this._rel; }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'src' && newValue !== oldValue) {
@@ -60,6 +57,15 @@ class ProfileCard extends HTMLElement {
       this._alt = newValue;
       this._updateImage();
     }
+    if (name === 'target' && newValue !== oldValue) {
+      this._target = newValue;
+      this._updateLink();
+    }
+
+    if (name === 'rel' && newValue !== oldValue) {
+      this._rel = newValue;
+      this._updateLink();
+    }
   }
 
   connectedCallback() {
@@ -67,21 +73,21 @@ class ProfileCard extends HTMLElement {
     this._updateLink(this.href);
   }
 
-  _updateImage(src) {
+  _updateImage() {
     const img = this.shadowRoot.querySelector('.profile-image');
     if (img) {
-      img.src = src || '';
+      img.src = this._src || '';
       img.alt = this._alt || 'Profile Image';
     }
   }
 
-  _updateLink(newValue) {
+  _updateLink() {
     const card = this.shadowRoot.querySelector('.profile-card');
     if (card) {
-      if (newValue) {
-        card.href = newValue;
-        card.target = '_blank';
-        card.rel = 'noopener noreferrer';
+      if (this._href) {
+        card.href = this._href;
+        card.target = this._target || '_blank'; 
+        card.rel = this._rel || 'noopener noreferrer'; 
       } else {
         card.removeAttribute('href');
         card.removeAttribute('target');
