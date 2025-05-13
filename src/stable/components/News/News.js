62992d463ebc75e52eb205f27c65808a508b0061
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-05-13 08:17:04 -0400

    Updated template for component

--- src/stable/components/News/News.js
+++ src/stable/components/News/News.js
@@ -6,7 +6,21 @@ template.innerHTML = `
 ${styles}
 </style>
 
-<div>
+<div class="news-card">
+
+<div class="news-header">
+<h3 class="news-title">
+  <slot name="news-title" id="newsTitleSlot">Default news title</slot>
+</h3>
+<span class="chevron"></span>
+</div>
+
+<div class="news-meta">
+   <span id="news-date"></span>
+      <div class="news-tags">
+        <slot name="tags" id="tagsSlot"></slot>
+      </div>
+</div>
 
 </div>
 `;
@@ -14,6 +28,28 @@ ${styles}
 class News extends HTMLElement {
   constructor() {
     super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  static get observedAttributes(){
+    return['datetime'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue){
+    if(name === 'date'){
+      this.formatDate(newValue);
+    }
+  }
+
+  connectedCallback(){
+  const date = this.getAttribute('date');
+  this.formatDate(date);
+  }
+
+
+  formatDate() {
+
   }
 }
 
