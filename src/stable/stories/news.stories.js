6cae3f2a2b5b713b55b3e6979b02bf9c01dba564
Shakira Reid-Thomas <shakira.reidthomas@detroitmi.gov>
2025-06-23 17:55:16 -0400

    Removed old code

--- src/stable/stories/news.stories.js
+++ src/stable/stories/news.stories.js
@@ -1,25 +1,3 @@
-// import { html } from 'lit-html';
-// import '../components/News/cod-news';
-// import '../components/Tag/cod-tag';
-
-// export default {
-//   tags: ['stable'],
-//   title: 'Components/News',
-// };
-
-// export const Basic = {
-//   render: () => html`
-//     <cod-news datetime="June 9, 2025">
-//       <span slot="news-title"
-//         >Today's Headlines: Lorem ipsum dolor sit amet, consectetur adipiscing
-//         elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-//         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-//         ut aliquip ex ea commodo consequat.</span
-//       >
-//       <cod-tag slot="tags"><span slot="label">Breaking News</span></cod-tag>
-//     </cod-news>
-//   `,
-// };
 import { html } from 'lit-html';
 import { expect } from '@storybook/test';
 import { userEvent } from '@storybook/test';
